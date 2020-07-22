1b7fd5fb7fde175bea4b52873b574193a1e01f45
Pridecoo <pridecoo@gmail.com>
2020-07-22 22:07:20 +0300

    feed and stats done

--- app/js/main.js
+++ app/js/main.js
@@ -7,9 +7,20 @@ $(function(){
     draggable: false,
     prevArrow: '<button class="slick-arrow slick-arrow--prev button" type="button"><span class="lnr lnr-chevron-left"></span></button>',
     nextArrow: '<button class="slick-arrow slick-arrow--next button" type="button"><span class="lnr lnr-chevron-right"></span></button>',
-    appendArrows: '.arrows-wrapper',
+    appendArrows: '.weekly__arrows-wrapper',
   });
 
+  $('.feed__items').slick({
+    arrows: true,
+    dots: false,
+    infinite: true,
+    draggable: false,
+    prevArrow: '<button class="slick-arrow slick-arrow--prev feed__arrow button" type="button"><span class="lnr lnr-chevron-left"></span></button>',
+    nextArrow: '<button class="slick-arrow slick-arrow--next feed__arrow button" type="button"><span class="lnr lnr-chevron-right"></span></button>',
+    appendArrows: '.feed__arrows-wrapper',
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  });
 
   $('.product__stars').rateYo({
     rating: 4.5,
